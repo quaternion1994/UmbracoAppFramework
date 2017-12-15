981cbbbde03ed7f273f5bf62955bb6daad91564a
VladByteAnt <vlad@byteant.com>
2017-12-15 20:49:11 +0200

    layout and validation

--- testumbracocontactform/testumbracocontactform.Bussiness/ViewModels/ContactFormViewModel.cs
+++ testumbracocontactform/testumbracocontactform.Bussiness/ViewModels/ContactFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Mail;
 
 namespace testumbracocontactform.Bussiness.ViewModels
@@ -6,11 +7,19 @@ namespace testumbracocontactform.Bussiness.ViewModels
     public class ContactFormViewModel
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Subject { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Phone { get; set; }
         public string Message { get; set; }
+
         public SmtpStatusCode MessageStatus { get; set; }
 
         public IEnumerable<string> Recepients { get; set; }
